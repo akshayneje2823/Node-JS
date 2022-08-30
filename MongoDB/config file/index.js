62b897f857543ce94742fe135657cffe161146bc
Akshay Neje <akshayneje2823@gmail.com>
2022-08-30 12:23:19 +0530

    Config Setting

---
+++ MongoDB/config file/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config({path:'./config.env'})
+
+app.get('/',(req,res)=>{
+    res.send("<h1>Root File</h1>");
+});
+
+app.listen(process.env.PORT,(err)=>{
+    console.log(process.env.PORT)
+    console.log(`server is running on port ${process.env.PORT}`);
+    if (err) throw err
+})
+
+
+
+
+
+
+
+ 
+
+
+// app.listen(4000,(err)=>{
+//     console.log(err);
+//     console.log('server is running on port 4000')  
+// })
