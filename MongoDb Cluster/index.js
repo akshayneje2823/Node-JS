4d4a1c505b625619c97cad03585c3b954812f582
Akshay Neje <akshayneje2823@gmail.com>
2023-05-23 23:34:24 +0530

    Mongo DB

--- MongoDb Cluster/index.js
+++ MongoDb Cluster/index.js
@@ -1,28 +1,39 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const { MongoClient } = require('mongodb')
+const os = require('os');
+
+
+// const { MongoClient } = require('mongodb')
 const app = express()
 
+// os module
+// console.log(os.hostname());
+console.log(os.version());
+console.log(os.freemem()/(1024*1024*1024));
+console.log(os.arch());
+console.log(os.platform());
+console.log(os.userInfo());
 
-// mongoose.connect("", {
-//     useUnifiedTopology: true,
-//     useNewUrlParser: true,
-// }).then((response) => {
-//     console.log(response)
-//     console.log("Mongodb connection successfull")
-// }).catch(() => {
-//     console.log("Unable to Connect")
-// });
 
-const url = "mongodb://localhost:27017";
-const client = new MongoClient(url);
+mongoose.connect("", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+}).then((response) => {
+    console.log(response)
+    console.log("Mongodb connection successful")
+}).catch(() => {
+    console.log("Unable to Connect")
+});
 
-async function mongoConnection() {
-    let connect = await client.connect();
-    let dbName = connect.db("new-DB");
+// const url = "mongodb://localhost:27017";
+// const client = new MongoClient(url);
+
+// async function mongoConnection() {
+//     let connect = await client.connect();
+//     let dbName = connect.db("new-DB");
     
-}
-mongoConnection()
+// }
+// mongoConnection()
 
 
 
